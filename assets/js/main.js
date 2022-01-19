d043ece867fb978f3e4332499700996c31eae617
FOR3ST321 <mkelvin3321@gmail.com>
2022-01-19 21:59:27 +0700

    validation bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,58 +1,68 @@
-function validate(fields) {
+function validate(formData) {
     let data = {
-        isValid : true,
-        errorMsg : []
-    }
+        isValid: true,
+        errorMsg: [],
+    };
 
-    if(fields.name === 'name'){
-        if(fields.value.length < 3){
-            data.isValid = false;
-            data.errorMsg.push({
-                field : 'name',
-                message : 'Name must be at least 3 characters!'
-            })
+    formData.forEach((fields) => {
+        if (fields.name === "name") {
+            if (fields.value.length < 3) {
+                data.isValid = false;
+                data.errorMsg.push({
+                    field: "name",
+                    message: "Name must be at least 3 characters!",
+                });
+            }
         }
-    }
 
-    if(fields.name === 'age'){
-        if(parseInt(fields.value) < 10){
-            data.isValid = false;
-            data.errorMsg.push({
-                field : 'age',
-                message : 'Age must be at least 10 years old!'
-            })
+        if (fields.name === "age") {
+            if (parseInt(fields.value) < 10) {
+                data.isValid = false;
+                data.errorMsg.push({
+                    field: "age",
+                    message: "Age must be at least 10 years old!",
+                });
+            }
         }
-    }
 
-    if(fields.name === 'gender'){
-        if(fields.value === ''){
-            data.isValid = false;
-            data.errorMsg.push({
-                field : 'gender',
-                message : 'Please select your gender!'
-            })
+        if (fields.name === "gender") {
+            if (fields.value === "") {
+                data.isValid = false;
+                data.errorMsg.push({
+                    field: "gender",
+                    message: "Please select your gender!",
+                });
+            }
         }
-    }
 
-    if(fields.name === 'phone'){
-        if(!fields.value.startsWith('+81') && (fields.value.substr(3).length < 11)){
-            data.isValid = false;
-            data.errorMsg.push({
-                field : 'phone',
-                message : 'Phone must starts with “+81” and must be 11 digits length'
-            })
+        if (fields.name === "phone") {
+            if (
+                !fields.value.startsWith("+81") ||
+                fields.value.substr(3).length < 11
+            ) {
+                data.isValid = false;
+                data.errorMsg.push({
+                    field: "phone",
+                    message:
+                        "Phone must starts with “+81” and must be 11 digits length",
+                });
+            }
         }
-    }
 
-    if(fields.name === 'email'){
-        if(!fields.value.endsWith(".com") && !(fields.value.startsWith('@')) && (fields.value.split('@').length != 1)){
-            data.isValid = false;
-            data.errorMsg.push({
-                field : 'email',
-                message : 'Email must be valid!'
-            })
+        if (fields.name === "email") {
+            if (
+                !fields.value.endsWith(".com") ||
+                !fields.value.startsWith("@") ||
+                fields.value.split("@").length != 1
+            ) {
+                data.isValid = false;
+                data.errorMsg.push({
+                    field: "email",
+                    message: "Email must be valid!",
+                });
+            }
         }
-    }
+    });
 
     return data;
 }
@@ -67,8 +77,5 @@ $(document).ready(function () {
 $("#joinForm").submit((e) => {
     e.preventDefault();
     const data = $("#joinForm").serializeArray();
-    // console.log(data);
-    $.each(data, function (indexInArray, valueOfElement) {
-        console.log(validate(valueOfElement));
-    });
+    console.log(validate(data));
 });
