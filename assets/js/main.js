f1badbbdd4f5e3df7a275f6a1d7b890abfc93c28
christopherkesuma <dnlkusuma1@gmail.com>
2022-01-21 19:15:16 +0700

    kerangka web

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,81 +1,85 @@
 function validate(formData) {
-    let data = {
-        isValid: true,
-        errorMsg: [],
-    };
+  let data = {
+    isValid: true,
+    errorMsg: [],
+  };
 
-    formData.forEach((fields) => {
-        if (fields.name === "name") {
-            if (fields.value.length < 3) {
-                data.isValid = false;
-                data.errorMsg.push({
-                    field: "name",
-                    message: "Name must be at least 3 characters!",
-                });
-            }
-        }
+  formData.forEach((fields) => {
+    if (fields.name === "name") {
+      if (fields.value.length < 3) {
+        data.isValid = false;
+        data.errorMsg.push({
+          field: "name",
+          message: "Name must be at least 3 characters!",
+        });
+      }
+    }
 
-        if (fields.name === "age") {
-            if (parseInt(fields.value) < 10) {
-                data.isValid = false;
-                data.errorMsg.push({
-                    field: "age",
-                    message: "Age must be at least 10 years old!",
-                });
-            }
-        }
+    if (fields.name === "age") {
+      if (parseInt(fields.value) < 10) {
+        data.isValid = false;
+        data.errorMsg.push({
+          field: "age",
+          message: "Age must be at least 10 years old!",
+        });
+      }
+    }
 
-        if (fields.name === "gender") {
-            if (fields.value === "") {
-                data.isValid = false;
-                data.errorMsg.push({
-                    field: "gender",
-                    message: "Please select your gender!",
-                });
-            }
-        }
+    if (fields.name === "gender") {
+      if (fields.value === "") {
+        data.isValid = false;
+        data.errorMsg.push({
+          field: "gender",
+          message: "Please select your gender!",
+        });
+      }
+    }
 
-        if (fields.name === "phone") {
-            if (
-                !fields.value.startsWith("+81") ||
-                fields.value.substr(3).length < 11
-            ) {
-                data.isValid = false;
-                data.errorMsg.push({
-                    field: "phone",
-                    message:
-                        "Phone must starts with “+81” and must be 11 digits length",
-                });
-            }
-        }
+    if (fields.name === "phone") {
+      if (
+        !fields.value.startsWith("+81") ||
+        fields.value.substr(3).length < 11
+      ) {
+        data.isValid = false;
+        data.errorMsg.push({
+          field: "phone",
+          message: "Phone must starts with “+81” and must be 11 digits length",
+        });
+      }
+    }
 
-        if (fields.name === "email") {
-            if (
-                !fields.value.endsWith(".com") ||
-                !fields.value.startsWith("@") ||
-                fields.value.split("@").length != 1
-            ) {
-                data.isValid = false;
-                data.errorMsg.push({
-                    field: "email",
-                    message: "Email must be valid!",
-                });
-            }
-        }
-    });
+    if (fields.name === "email") {
+      if (
+        !fields.value.endsWith(".com") ||
+        !fields.value.startsWith("@") ||
+        fields.value.split("@").length != 1
+      ) {
+        data.isValid = false;
+        data.errorMsg.push({
+          field: "email",
+          message: "Email must be valid!",
+        });
+      }
+    }
+  });
 
-    return data;
+  return data;
 }
 
 $(document).ready(function () {
-    $("#test").text(programs[0].name);
+  $(".navbar").css("display", "none");
+  $("#dropdown").click(function () {
+    $(".navbar").slideToggle();
+  });
 
-    $("#test-blog").html(blog[0].description);
-    // console.log(programs)
+  $("#test").text(programs[0].name);
+
+  $("#test-blog").html(blog[0].description);
+  // console.log(programs)
 });
 
 $("#joinForm").submit((e) => {
-    e.preventDefault();
-    const data = $("#joinForm").serializeArray();
-    console.log(validate(data));
+  e.preventDefault();
+  const data = $("#joinForm").serializeArray();
+  console.log(validate(data));
 });
