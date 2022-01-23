9d8b9611952a916d540b5bdad39275b89ef2829b
FOR3ST321 <mkelvin3321@gmail.com>
2022-01-23 21:32:58 +0700

    class hover

--- assets/js/classes.js
+++ assets/js/classes.js
@@ -2,9 +2,11 @@ $(document).ready(function () {
   programs.forEach(display);
   function display(item) {
     $(".content-section").append(`
-          <button class="content-item content-item-${item.id}">
+          <button class="content-item content-item-${item.id}" >
             <div class="class-name">${item.name}</div>
             <div class="class-duration">${item.duration}</div>
+            <div class="class-details">Details ></div>
+
           </button>
         `);
     $(`.content-item-${item.id}`).css(
@@ -12,4 +14,28 @@ $(document).ready(function () {
       `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),url(${item.image})`
     );
   }
-});
+
+
+
+  $(".content-item").mouseenter(function(){
+    $(this).children('.class-details').animate({
+      opacity: '1'
+    }, 100)
+    $(this).css({
+      border: "0px solid #99bbff"
+    }).animate({
+      borderWidth : '6'
+    }, 100)
+  })
+
+  $(".content-item").mouseleave(function(){
+    $(this).children('.class-details').animate({
+      opacity: '0'
+    }, 100)
+    $(this).css({
+      border: "6px solid #99bbff"
+    }).animate({
+      borderWidth : '0'
+    }, 100)
+  })
+})
