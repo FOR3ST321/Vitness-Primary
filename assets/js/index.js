cf96b5b9ea80d819d27f9538d99b84d404cec0a5
christopherkesuma <dnlkusuma1@gmail.com>
2022-01-26 20:25:20 +0700

    benerin responsive slider

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,24 @@
 $(document).ready(function () {
-    // class
-    // for(let i = programs.length-1;i>=programs.length-4;i--){
-    //   let item = programs[i];
-    //   $(".class-content").append(`
-    //   <button class="class-content-item">
-    //     <div class="class-pict" id="class-pict-${item.id}"></div>
-    //     <div class="class-desc">
-    //       <div class="class-name">${item.name}</div>
-    //       <div class="class-time">${item.duration}</div>
-    //     </div>
-    //   </button>
-    // `);
-    //     $(`#class-pict-${item.id}`).css(
-    //         "background-image",
-    //         `url(${item.image})`
-    //     );
-    // }
-    programs.forEach(classFunction);
-    function classFunction(item) {
-        $(".class-content").append(`
+  // class
+  // for(let i = programs.length-1;i>=programs.length-4;i--){
+  //   let item = programs[i];
+  //   $(".class-content").append(`
+  //   <button class="class-content-item">
+  //     <div class="class-pict" id="class-pict-${item.id}"></div>
+  //     <div class="class-desc">
+  //       <div class="class-name">${item.name}</div>
+  //       <div class="class-time">${item.duration}</div>
+  //     </div>
+  //   </button>
+  // `);
+  //     $(`#class-pict-${item.id}`).css(
+  //         "background-image",
+  //         `url(${item.image})`
+  //     );
+  // }
+  programs.forEach(classFunction);
+  function classFunction(item) {
+    $(".class-content").append(`
       <button class="class-content-item">
         <div class="class-pict" id="class-pict-${item.id}"></div>
         <div class="class-desc">
@@ -27,16 +27,13 @@ $(document).ready(function () {
         </div>
       </button>
     `);
-        $(`#class-pict-${item.id}`).css(
-            "background-image",
-            `url(${item.image})`
-        );
-    }
+    $(`#class-pict-${item.id}`).css("background-image", `url(${item.image})`);
+  }
 
-    // blog
-    blog.forEach(blogFunction);
-    function blogFunction(item) {
-        $(".blog-content").append(`
+  // blog
+  blog.forEach(blogFunction);
+  function blogFunction(item) {
+    $(".blog-content").append(`
       <a href="./blog-detail.html?id=${item.id}" class="blog-content-item">
         <div class="blog-item-img" id="blog-item-img-${item.id}">
           <div class="blog-category">${item.category}</div>
@@ -48,17 +45,17 @@ $(document).ready(function () {
         </div>
       </a>
     `);
-        $(`#blog-item-img-${item.id}`).css(
-            "background-image",
-            `url(${item.image})`
-        );
-    }
+    $(`#blog-item-img-${item.id}`).css(
+      "background-image",
+      `url(${item.image})`
+    );
+  }
 
-    //slider
-    let firstimg = true;
-    slider.forEach(function (item) {
-        $(".sliders-content").append(
-            `
+  //slider
+  let firstimg = true;
+  slider.forEach(function (item) {
+    $(".sliders-content").append(
+      `
       <div class="slider-container" id="slide-${item.id}">
             <div class="slider-img-container">
                 <img src="${item.image}" alt="" class="slider-img"/>
@@ -67,75 +64,75 @@ $(document).ready(function () {
                 <p class="slider-title">${item.title}</p>
                 <p class="slider-desc">${item.description}</p>
                 ${
-                    item.hasBtn
-                        ? `<button data-url="${item.btnUrl}" class="slider-btn"> Detail >></button>`
-                        : ""
+                  item.hasBtn
+                    ? `<button data-url="${item.btnUrl}" class="slider-btn"> Detail >></button>`
+                    : ""
                 }
             </div>
         </div>
       `
-        );
+    );
 
-        if (firstimg) {
-            firstimg = false;
-        } else {
-            $("#slide-" + item.id).hide();
-        }
-    });
-
-    let slideIndex = 1;
-    let first = true;
-    let currSlide = "";
-    // slider.forEach((el) => {
-    //     $("#slide-" + el.id).show();
-    // });
-
-    // var interval = window.setInterval(rotateSlides, 3000);
-
-    // function rotateSlides() {
-    //   var $firstSlide = $('.slider').find('div:first');
-    //   // console.log($secondSlide)
-    //   var width = $firstSlide.width();
-    //   // console.log(width);
-
-    //   $firstSlide.animate({marginLeft: -width}, 1000, function(){
-    //     // What to do after the animation
-    //   })
-    // }
-
-    showSlides();
-
-    function showSlides() {
-        if (first) {
-            first = false;
-            slider.forEach((el) => {
-                $("#slide-" + el.id).hide();
-            });
-            $("#slide-" + slideIndex).show();
-            currSlide = "#slide-" + slideIndex;
-        } else {
-            //geser kiri minus yang sekarang, geser kiri 0 yang mau next
-            $(currSlide)
-                .css({ opacity: 1, marginLeft:'0' })
-                .animate({ opacity: 0, marginLeft:'100px' }, 700, function () {
-                    //change index
-                    slideIndex++;
-                    if (slideIndex > slider.length) {
-                        slideIndex = 1;
-                    }
-                    $(currSlide).hide();
-                    $("#slide-" + slideIndex).show();
-                    $("#slide-" + slideIndex)
-                        .css({ opacity: 0 , marginLeft:'-100px'})
-                        .animate({ opacity: 1, marginLeft:'0px' }, 700);
-                    currSlide = "#slide-" + slideIndex;
-                });
-        }
-        setTimeout(showSlides, 7000);
+    if (firstimg) {
+      firstimg = false;
+    } else {
+      $("#slide-" + item.id).hide();
     }
+  });
 
-    $(".slider-btn").on("click", function () {
-        // console.log();
-        window.location.replace($(this).data("url"));
-    });
+  let slideIndex = 1;
+  let first = true;
+  let currSlide = "";
+  // slider.forEach((el) => {
+  //     $("#slide-" + el.id).show();
+  // });
+
+  // var interval = window.setInterval(rotateSlides, 3000);
+
+  // function rotateSlides() {
+  //   var $firstSlide = $('.slider').find('div:first');
+  //   // console.log($secondSlide)
+  //   var width = $firstSlide.width();
+  //   // console.log(width);
+
+  //   $firstSlide.animate({marginLeft: -width}, 1000, function(){
+  //     // What to do after the animation
+  //   })
+  // }
+
+  showSlides();
+
+  function showSlides() {
+    if (first) {
+      first = false;
+      slider.forEach((el) => {
+        $("#slide-" + el.id).hide();
+      });
+      $("#slide-" + slideIndex).show();
+      currSlide = "#slide-" + slideIndex;
+    } else {
+      //geser kiri minus yang sekarang, geser kiri 0 yang mau next
+      $(currSlide)
+        .css({ opacity: 1, marginLeft: "0" })
+        .animate({ opacity: 0, marginLeft: "200px" }, 700, function () {
+          //change index
+          slideIndex++;
+          if (slideIndex > slider.length) {
+            slideIndex = 1;
+          }
+          $(currSlide).hide();
+          $("#slide-" + slideIndex).show();
+          $("#slide-" + slideIndex)
+            .css({ opacity: 0, marginLeft: "-200px" })
+            .animate({ opacity: 1, marginLeft: "0px" }, 700);
+          currSlide = "#slide-" + slideIndex;
+        });
+    }
+    setTimeout(showSlides, 7000);
+  }
+
+  $(".slider-btn").on("click", function () {
+    // console.log();
+    window.location.replace($(this).data("url"));
+  });
 });
