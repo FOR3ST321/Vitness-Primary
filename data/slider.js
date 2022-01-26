27d9e654af97fa6843c6453ddf02cfcb61cf070f
FOR3ST321 <mkelvin3321@gmail.com>
2022-01-26 14:27:59 +0700

    slider ?

---
+++ data/slider.js
@@ -0,0 +1,26 @@
+const slider = [
+    {
+        id : 1,
+        title : 'Welcome To Vitness Primary!',
+        description : `This is the best gym you can find in the town!`,
+        hasBtn : false,
+        btnUrl : null,
+        image : './assets/img/welcome.jpg',
+    },
+    {
+        id : 2,
+        title : 'Vitness Primary 1st Anniv!',
+        description : `We celebrate our anniversary on 05 february 2022!`,
+        hasBtn : true,
+        btnUrl : './blog-detail.html?id=5',
+        image : './assets/img/blog/5.jpg',
+    },
+    {
+        id : 3,
+        title : 'New Yoga Class Announced!',
+        description : `Join to vitness and follow this class!`,
+        hasBtn : true,
+        btnUrl : './classes.html',
+        image : './assets/img/class/1.png',
+    },
+]
